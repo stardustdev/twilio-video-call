f5ac1c425bf8545fc9eb6818d93182892ffe6bb8
Stardust DEV <stardustdev@outlook.com>
2020-06-02 21:42:35 -0700

    Remove dummy code

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -5,7 +5,7 @@ import { User } from 'firebase';
 export interface StateContextType {
   error: TwilioError | null;
   setError(error: TwilioError | null): void;
-  getToken(name: string, room: string, passcode?: string): Promise<string>;
+  getToken(name: string, room: string): Promise<string>;
   user?: User | null | { displayName: undefined; photoURL: undefined; passcode?: string };
   signIn?(passcode?: string): Promise<void>;
   signOut?(): Promise<void>;
